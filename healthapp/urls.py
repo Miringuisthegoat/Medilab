4f3f6aeaf850adcf9c08e3f23b159161276a2ff6
perizdonah3 <perizdonah3@gmail.com>
2026-02-26 13:48:03 +0300

    new commit

--- healthapp/urls.py
+++ healthapp/urls.py
@@ -9,9 +9,16 @@ urlpatterns = [
     path('', views.home,name='home'),
 
     path('starter', views.starter,name='starter'),
+
     path('About', views.About,name='About'),
+
     path('appointment', views.appointment,name='appointment'),
+
     path('show', views.show,name='show'),
+
+    path('delete/<int:id>', views.delete,name='delete'),
+
+    path('edit/<int:id>', views.edit,name='edit'),
 
 
 
--- healthapp/views.py
+++ healthapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from healthapp.models import *
+
+
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
@@ -31,3 +33,25 @@ def appointment(request):
 def show(request):
    allapointments = Myappointment.objects.all()
    return render(request, 'show.html', {'appointments': allapointments})
+
+def delete(request, id):
+       appointment = Myappointment.objects.get(id=id)
+       appointment.delete()
+       return redirect('/show')
+
+def edit(request, id):
+    editappointment = get_object_or_404(Myappointment, id=id)
+
+    if request.method == 'POST':
+        editappointment.name = request.POST.get('name')
+        editappointment.email = request.POST.get('email')
+        editappointment.phone = request.POST.get('phone')
+        editappointment.datetime = request.POST.get('datetime')
+        editappointment.department = request.POST.get('department')
+        editappointment.doctor = request.POST.get('doctor')
+        editappointment.message = request.POST.get('message')
+
+        editappointment.save()
+        return redirect('/show')
+    else:
+        return render(request, 'edit.html', {'editappointment': editappointment})   
